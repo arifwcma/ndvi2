2bed89d7fc89e9738ad9cb62fa3091a3e83aff9f
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-09-16 15:11:36 +1000

    done

--- server.js
+++ server.js
@@ -14,33 +14,8 @@ ee.data.authenticateViaPrivateKey(privateKey, () => {
   });
 });
 
-app.get("/ndvi", (req, res) => {
-  const region = ee.Geometry.Polygon([
-    [
-      [140.963783171735315, -37.368096687169292],
-      [143.363961290248056, -37.368096687169292],
-      [143.363961290248056, -35.656805505972457],
-      [140.963783171735315, -35.656805505972457],
-      [140.963783171735315, -37.368096687169292],
-    ],
-  ]);
-
-  const image = ee
-    .ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
-    .filterDate("2023-06-01", "2023-06-30")
-    .filterBounds(region)
-    .median()
-    .normalizedDifference(["B8", "B4"])
-    .visualize({ min: 0, max: 1, palette: ["brown", "yellow", "green"] });
-
-  image.getThumbURL(
-    { dimensions: 512, region: region, format: "png" },
-    (url, error) => {
-      if (error) res.status(500).send(error);
-      else res.json({ url });
-    }
-  );
-});
+const ndviRoutes = require("./routes/ndvi");
+app.use("/ndvi", ndviRoutes);
 
 app.listen(3001, () =>
   console.log("Server running on http://localhost:3001")
