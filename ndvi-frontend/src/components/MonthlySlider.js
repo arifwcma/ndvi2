ae42b62f3fc11076944f47d157723d657d8d586c
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-09-17 17:03:47 +1000

    done

--- ndvi-frontend/src/components/MonthlySlider.js
+++ ndvi-frontend/src/components/MonthlySlider.js
@@ -6,9 +6,9 @@ import L from "leaflet"
 delete L.Icon.Default.prototype._getIconUrl
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: "/marker-icon-2x.png",
-    iconUrl: "/marker-icon.png",
-    shadowUrl: "/marker-shadow.png"
+    iconRetinaUrl: "/images/marker-icon-2x.png",
+    iconUrl: "/images/marker-icon.png",
+    shadowUrl: "/images/marker-shadow.png"
 })
 
 
